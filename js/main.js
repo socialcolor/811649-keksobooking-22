38c94dddbc18742bb65d53ea1f4352828c8ced91
Gevork <64646814+socialcolor@users.noreply.github.com>
2021-02-02 04:35:24 +0300

    испраивл ошибки

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const getRandomNumber = function (min, max) {
-  if (min < max && (min > 0 && max > 0)) {
+  min = Math.floor(min); // если вдруг ввели дробное число, то округляем его первого знака
+  max = Math.floor(max); // хотел тут использовать округления в большую сторону, но подумал что это будет некорректно
+  if (min < max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   } else if (min === max) {
     return min;
@@ -10,13 +12,10 @@ const getRandomNumber = function (min, max) {
 };
 
 const getRandomNumberFloat = function (min, max, float = 2) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  if (min < max && (min > 0 && max > 0)) {
-    return (Math.random() * (max - min + 1) + min).toFixed(float);
+  if (min < max) {
+    return +(Math.random() * (max - min + 0.1) + min).toFixed(float); //Тут есть проблема, что если знаков после запятой будет больше одного, то максимальное число станет некорректным
   } else if (min === max) {
     return min;
   } // esLint не дает тут поставить точку с зяпятой
   return 0;
 };
-
