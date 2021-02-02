ebcdc9e0e21c4d97927e0b53121c15d47cb5288c
Gevork <64646814+socialcolor@users.noreply.github.com>
2021-02-02 12:18:39 +0300

    Проверка на положительное число

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@
 const getRandomNumber = function (min, max) {
   min = Math.floor(min); // если вдруг ввели дробное число, то округляем его первого знака
   max = Math.floor(max); // хотел тут использовать округления в большую сторону, но подумал что это будет некорректно
-  if (min < max) {
+  if (min < max && (min || max) > 0) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   } else if (min === max) {
     return min;
   } // esLint не дает тут поставить точку с зяпятой
-  return 0;
+  return 'Error';
 };
 
 const getRandomNumberFloat = function (min, max, float = 2) {
-  if (min < max) {
+  if (min < max && (min || max) > 0) {
     return +(Math.random() * (max - min) + min).toFixed(float);
   } else if (min === max) {
     return min;
   } // esLint не дает тут поставить точку с зяпятой
-  return 0;
+  return 'Error';
 };
