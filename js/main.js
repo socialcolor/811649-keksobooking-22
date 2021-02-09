347e79ae73201671f5d4904645b140296c8bb63a
Gevork <64646814+socialcolor@users.noreply.github.com>
2021-02-09 22:23:45 +0300

    Исправил функцию

--- js/main.js
+++ js/main.js
@@ -1,69 +1,80 @@
 'use strict';
+const OFFER_TITLES = ['Лучшее предложения в Токие', 'Снять номер дешево', 'Скидки до конца недели'];
+const OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const OFFER_TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const OFFER_TIMES = ['12:00', '13:00', '14:00'];
+const OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const OFFER_COUNT = 10;
+const USER_AVATARS = 8;
+const LOCATION_X_MIN = 35.65000;
+const LOCATION_X_MAX = 35.70000;
+const LOCATION_Y_MIN = 139.70000;
+const LOCATION_Y_MAX = 139.80000;
+const LOCATION_FLOAT = 5;
+const PRICE_MIN = 1000;
+const PRICE_MAX = 10000;
+const ROOM_MAX = 5;
+const GUEST_MAX = 5;
 
 const getRandomNumber = (min, max) => {
-  min = Math.floor(min); // если вдруг ввели дробное число, то округляем его первого знака
-  max = Math.floor(max); // хотел тут использовать округления в большую сторону, но подумал что это будет некорректно
+  min = Math.floor(min);
+  max = Math.floor(max);
   if (min < max && (min && max) >= 0) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   } else if (min === max) {
     return min;
-  } // esLint не дает тут поставить точку с зяпятой
+  }
   return 'Error';
-};
+}
 
 const getRandomNumberFloat = (min, max, float = 2) => {
   if (min < max && (min && max) >= 0) {
     return +(Math.random() * (max - min) + min).toFixed(float);
   } else if (min === max) {
     return min;
-  } // esLint не дает тут поставить точку с зяпятой
+  }
   return 'Error';
+}
+
+const getRandomElement = (array) => {
+
+  let currentFeatures = [];
+  let featuresLength = getRandomNumber(1, array.length);
+  for (let i = 0; i < featuresLength - 1; i++) {
+    let rundomFeatures = getRandomNumber(0, array.length - 1);
+    let checkFeatres = currentFeatures.some((value) => value === array[rundomFeatures]);
+    if (!checkFeatres) {
+      currentFeatures.push(array[rundomFeatures]);
+    }
+  }
+  return currentFeatures;
 };
 
-const getNearbyOffers = () => {
-  let nearbyOffers = new Array(10).fill({});
-  nearbyOffers.forEach((value, index, array) => {
-    let titleList = ['Лучшее предложения в Токие', 'Снять номер дешево', 'Скидки до конца недели']; // Список офферов
-    let typesList = ['palace', 'flat', 'house', 'bungalow']; // Список типов мест
-    let timeList = ['12:00', '13:00', '14:00'] // Список чекина и чекаута
-    const getRandimFeatures = () => {
-      let featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']; // Список особенностей
-      let currentFeatures = []; // Массив особенностей
-      let featuresLength = getRandomNumber(1, 6); // Определяем количество особенностей
-      for (let i = 0; i < featuresLength; i++) {
-        let rundomFeatures = getRandomNumber(0, 5); // Выбираем случайную особенность
-        let checkFeatres = currentFeatures.some((value) => value === featuresList[rundomFeatures]); // Проверяем есть ли уже такая особенность
-        if (!checkFeatres) {
-          currentFeatures.push(featuresList[rundomFeatures]);
-        }
-      }
-      return currentFeatures;
-    };
-
-    array[index] = {
-      'author': {
-        'avatar': 'img/avatars/user' + '0' + getRandomNumber(1, 8) + '.png',
-      },
-      'offer': {
-        'title': titleList[getRandomNumber(0, 2)],
-        'adress': getRandomNumberFloat(35.65000, 35.70000, 5) + ', ' + getRandomNumberFloat(139.70000, 139.80000, 5),
-        'price': getRandomNumber(1000, 10000),
-        'type': typesList[getRandomNumber(0, 3)],
-        'rooms': getRandomNumber(1, 5),
-        'guests': getRandomNumber(1, 3),
-        'checkin': timeList[getRandomNumber(0, 2)],
-        'checkout': timeList[getRandomNumber(0, 2)],
-        'features': getRandimFeatures(),
-        'description': 'Хороший отел с хорошими ценами',
-        'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
-      },
-      'location': {
-        'x': getRandomNumberFloat(35.65000, 35.70000, 5),
-        'y': getRandomNumberFloat(139.70000, 139.80000, 5),
-      },
-    };
-  })
-  return nearbyOffers;
+const getNearbyOffers = function () {
+  let locationX = getRandomNumberFloat(LOCATION_X_MIN, LOCATION_X_MAX, LOCATION_FLOAT);
+  let locationY = getRandomNumberFloat(LOCATION_Y_MIN, LOCATION_Y_MAX, LOCATION_FLOAT);
+  return {
+    'author': {
+      'avatar': 'img/avatars/user0' + getRandomNumber(1, USER_AVATARS) + '.png',
+    },
+    'offer': {
+      'title': OFFER_TITLES[getRandomNumber(0, OFFER_TITLES.length - 1)],
+      'adress': `${locationX}, ${locationY}`,
+      'price': getRandomNumber(PRICE_MIN, PRICE_MAX),
+      'type': OFFER_TYPES[getRandomNumber(0, OFFER_TYPES.length - 1)],
+      'rooms': getRandomNumber(1, ROOM_MAX),
+      'guests': getRandomNumber(1, GUEST_MAX),
+      'checkin': OFFER_TIMES[getRandomNumber(0, OFFER_TIMES.length - 1)],
+      'checkout': OFFER_TIMES[getRandomNumber(0, OFFER_TIMES.length - 1)],
+      'features': getRandomElement(OFFER_FEATURES),
+      'description': 'Хороший отел с хорошими ценами',
+      'photos': OFFER_PHOTOS[getRandomNumber(0, OFFER_PHOTOS.length - 1)],
+    },
+    'location': {
+      'x': locationX,
+      'y': locationY,
+    },
+  };
 };
 
-getNearbyOffers();
+const nearbyOffers = new Array(OFFER_COUNT).fill(null).map(() => getNearbyOffers());
