957fcbeba0d2b7181b04e37ba4b237338c401c60
Gevork <64646814+socialcolor@users.noreply.github.com>
2021-02-17 14:18:30 +0300

    Исправления

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ import {genereteNearbyOffer} from './data.js';
 import {createOfferMarkup} from './offer.js';
 import './form.js';
 
-const mapCanvas = document.querySelector('#map-canvas');
 const OFFER_COUNT = 10;
+const mapCanvas = document.querySelector('#map-canvas');
 const nearbyOffers = genereteNearbyOffer(OFFER_COUNT);
 
 const offerMarkup = createOfferMarkup(nearbyOffers[0]);
