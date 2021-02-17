e8d1fb00137a3796ca7db89ff90c8c522dff3030
Gevork <64646814+socialcolor@users.noreply.github.com>
2021-02-17 11:00:38 +0300

    Сделал обработчики форм

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 import {genereteNearbyOffer} from './data.js';
 import {createOfferMarkup} from './offer.js';
+import './form.js';
 
 const mapCanvas = document.querySelector('#map-canvas');
 const OFFER_COUNT = 10;
 const nearbyOffers = genereteNearbyOffer(OFFER_COUNT);
 
 const offerMarkup = createOfferMarkup(nearbyOffers[0]);
-
 mapCanvas.appendChild(offerMarkup);
